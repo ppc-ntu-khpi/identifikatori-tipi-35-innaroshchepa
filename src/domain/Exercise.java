62b838bcb79620d208ba74ae739359fa9f67db05
innaroshchepa <62202381+innaroshchepa@users.noreply.github.com>
2020-04-22 14:29:30 +0300

    Update Exercise.java

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package domain;
 
+/**
+ *
+ * @author Инна
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    /**
+     * A method that converts a six-digit number represented by a decimal number into a binary system
+     * @param R integer
+     * @return specified binary number
+     */
+    public static String CalculateBinarySystem(int R){
+        return (String) (Integer.toBinaryString((int) R));
     }
-}
+    /**
+     * A method that converts a six-digit number represented by a decimal number into a eight-year system
+     * @param R integer 
+     * @return specified number in octal notation
+     */
+    public static String CalculateOctalSystem(int R){
+        return (String) (Integer.toOctalString((int) R));
+    }
+    /**
+     * A method that converts a six-digit number represented by a decimal number into a hexadecimal system
+     * @param R integer 
+     * @return specified number in hexadecimal notation
+     */
+    public static String CalculateHexadecimalSystem(int R){
+        return (String) (Integer.toHexString((int) R));
+    }
