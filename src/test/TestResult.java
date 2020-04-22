e36790f05bad90eafe83bfc2e1a13d07f0637442
innaroshchepa <62202381+innaroshchepa@users.noreply.github.com>
2020-04-22 14:30:18 +0300

    Update TestResult.java

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package test;
-
 import domain.Exercise;
-
+/**
+ *
+ * @author Инна
+ */
 public class TestResult {
 
     public static void main(String[] args) {
 
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        int R=999991;
+        if(R>=100000 && R<1000000)
+            {
+            System.out.println("Число "+R+" в двоичной системе: "+Exercise.CalculateBinarySystem(R)+"\nв восьмеричной системе: "+Exercise.CalculateOctalSystem(R)+"\nв шестнадцатеричной системе: "+Exercise.CalculateHexadecimalSystem(R));
+            }
+        else{
+            System.out.println("Число должно быть шестизначным!");
+            }
     }
 }
+    
+    
+    
+
+
