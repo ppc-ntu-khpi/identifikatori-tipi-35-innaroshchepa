5ee83e4f01f9e1aaa92f700c4607e7c4fe32be45
innaroshchepa <62202381+innaroshchepa@users.noreply.github.com>
2020-04-23 21:00:17 +0300

    Update TestResult.java

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,4 +1,5 @@
 /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -16,13 +17,14 @@ public class TestResult {
         int R=999991;
         if(R>=100000 && R<1000000)
             {
-            System.out.println("Число "+R+" в двоичной системе: "+Exercise.CalculateBinarySystem(R)+"\nв восьмеричной системе: "+Exercise.CalculateOctalSystem(R)+"\nв шестнадцатеричной системе: "+Exercise.CalculateHexadecimalSystem(R));
+            System.out.println("\r\n" +Exercise.Calculate(R)) ;
             }
         else{
             System.out.println("Число должно быть шестизначным!");
             }
     }
 }
+    
     
     
     
